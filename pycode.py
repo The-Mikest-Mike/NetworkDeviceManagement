a904cf30fe7ac95cca240570691def5b4cea64fd
MIKE <eng.miguelaz@gmail.com>
2023-09-23 16:56:46 -0700

    <refactor>(pep8):refactoring the code to follow pep8 style and CamelColde

--- pycode.py
+++ pycode.py
@@ -1,14 +1,18 @@
-class robot:
-    def __init__(self,name,color,brand,weight):
+# class
+class Robot:
+    def __init__(self, name, color, brand, weight):
+
         self.name = name
         self.color = color
         self.brand = brand
-        self.weight = weight
+        self.weight = float(weight)
 
     def introduce_self(self):
-        print("my characteristics are:"  '\n    name: ' + self.name + '\n    color: ' + self.color + '\n    brand: ' + self.brand + '\n    weight: ' + self.weight)
-    
-r1 = robot('romina','black','lefant','500')
+        #Print information about the robot's characteristics.
+        print(f"my characteristics are:\nName: {self.name}\nColor: {self.color}\nBrand: {self.brand}\nWeight: {self.weight} kg")
+
+# usage
+r1 = Robot('Romina', 'Black', 'Lefant', .500)
 r1.introduce_self()
-r2 = robot('alexa','gray','amazon','100')
+r2 = Robot('Alexa', 'Gray', 'Amazon', .100)
 r2.introduce_self()
