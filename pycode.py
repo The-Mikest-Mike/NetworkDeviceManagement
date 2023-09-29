aff3ca853ba80a2e63f4b0b3afa0e6c26983161c
MIKE <eng.miguelaz@gmail.com>
2023-09-28 20:05:20 -0700

    <feature1>(complete):completed feature1 code for building and test phases

--- pycode.py
+++ pycode.py
@@ -1,18 +1,29 @@
 # class
-class Robot:
-    def __init__(self, name, color, brand, weight):
+class Robot: 
+    number_of_robots = 0
 
+    def __init__(self, name, color, brand, weight):
         self.name = name
         self.color = color
         self.brand = brand
         self.weight = float(weight)
 
+        Robot.number_of_robots += 1
+
+    # print information about their characteristics
     def introduce_self(self):
-        #Print information about the robot's characteristics.
         print(f"my characteristics are:\nName: {self.name}\nColor: {self.color}\nBrand: {self.brand}\nWeight: {self.weight} kg")
 
-# usage
+        
+# add robots with their characteristics
 r1 = Robot('Romina', 'Black', 'Lefant', .500)
-r1.introduce_self()
 r2 = Robot('Alexa', 'Gray', 'Amazon', .100)
+
+# print number of robots. It will be updated as robots being added
+print(f"the current number of robots is: {Robot.number_of_robots}")
+   
+r1.introduce_self()
 r2.introduce_self()
+
+
+
