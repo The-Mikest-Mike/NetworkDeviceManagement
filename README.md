19e5b3e909eabfe178bf8ef988e78c4b593a7e0e
BOLA <bola@BOLAs-MacBook-Air.local>
2024-03-05 08:17:14 -0800

    <ref>(refactor):modularize code using json file with all devices

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # NetworkDeviceManagement
 
-Defines a Devices class that represents device's attributes.
-It showcases principles of object-oriented programming in Python, allowing for the creation of device instances.
-Also keeps track of the total number of device instances created and provides a method to print their individual characteristics.
-
 ## Features
 
 - Create device instances with different characteristics.
 - Track the total number of devices.
 - Print device characteristics.
+- Read Device Characteristics from JSON File
+- Flexible Device Initialization with various attributes like name, color, brand, weight, software version, and IP address, offering users the flexibility to define devices with diverse characteristics.
+- Modular Design: enhances code reusability, readability, and maintainability.
--- pycode.py
+++ pycode.py
@@ -1,21 +1,24 @@
-# class to represent network devices
+import json
+
+# Class to represent network devices
 class Device: 
-    # class variable to keep track of the number of devices
+    # Class variable to keep track of the number of devices
     number_of_devices = 0
-    # method to initialize a device with its attributes
+    
+    # Method to initialize a device with its attributes
     def __init__(self, name, color, brand, weight, swversion, ipaddress):
-        self.name = name # name of device
-        self.color = color # color of device
-        self.brand = brand # brand of device
-        self.weight = float(weight) # weight of device in kilograms
-        self.swversion = (swversion) # software version of device
-        self.ipaddress = (ipaddress) # ip address of device
+        self.name = name  # Name of device
+        self.color = color  # Color of device
+        self.brand = brand  # Brand of device
+        self.weight = float(weight)  # Weight of device in kilograms
+        self.swversion = swversion  # Software version of device
+        self.ipaddress = ipaddress  # IP address of device
 
-        Device.number_of_devices += 1 # increment the number of devices
+        Device.number_of_devices += 1  # Increment the number of devices
 
-    # method to return a description of the devices
+    # Method to return a description of the device
     def introduce_self(self):
-        '''Return a description of the device'''
+        '''Returns a description of the device'''
         introduce = f'Name: {self.name}\n'\
                     f'Color: {self.color}\n'\
                     f'Brand: {self.brand}\n'\
@@ -23,18 +26,41 @@ class Device:
                     f'SWVersion: {self.swversion}\n'\
                     f'IPAdress: {self.ipaddress}'
         return introduce
-        
-# instantiate devices with their characteristics
-d1 = Device('Romina', 'Black', 'Lefant', .500, '4.7.1', '192.168.1.2')
-d2 = Device('Alexa', 'Gray', 'Amazon', .100, '4.7.1', '192.168.1.3')
 
-# print current number of devices. It will be updated as they are being added
-print(f"the current number of devices is: {Device.number_of_devices}")
+# Read device characteristics from JSON file
+def read_devices_from_json(filename):
+    '''
+    Reads device characteristics from a JSON file and returns a list of Device objects.
+    Args:
+        filename (str): The name of the JSON file containing device information.
+    Returns:
+        list: A list of Device objects containing the characteristics of each device.
+    '''
+    devices = []  # Initialize an empty list to store Device objects
+    with open(filename, 'r') as file:  # Open the JSON file for reading
+        data = json.load(file)  # Load the JSON data from the file
+        for device_data in data:  # Iterate over each device's data in the JSON data
+            # Create a Device object using the device's data and append it to the devices list
+            device = Device(device_data['name'], device_data['color'], device_data['brand'], 
+                            device_data['weight'], device_data['swversion'], device_data['ipaddress'])
+            devices.append(device)  # Append the created Device object to the list
+    return devices  # Return the list of Device objects containing device characteristics
 
-# print information about the devices
-print('d1\n', d1.introduce_self(), '\n')
-print('d2\n', d2.introduce_self(), '\n')
+# Instantiate devices from JSON file
+devices = read_devices_from_json('network_devices.json')
 
+# Print current number of devices
+print(f"The current number of devices is: {Device.number_of_devices}")
 
-
+for i, device in enumerate(devices, 1):
+    '''
+    Iterates over the list of devices and prints information about each device.
+    Args:
+        devices (list): A list of Device objects containing the characteristics of each device.
+        i (int): Index of the device being printed, starting from 1.
+        device (Device): The Device object being printed.
+    '''
+    print(f"Device {i}:")  # Print the index of the device
+    print(device.introduce_self())  # Print the description of the device
+    print()  # Print an empty line for better readability
 
