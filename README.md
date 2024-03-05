6d9349fa8c384a32f492c5394b7b2a7fe4f0dea0
BOLA <bola@BOLAs-MacBook-Air.local>
2024-03-04 19:27:34 -0800

    <ref>(refactor):refactored code per jira

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# RobotsEmployeeDirectory
+# NetworkDeviceManagement
 
-This Python program defines a Robot class that represents robots with attributes such as name, color, brand, and weight. 
-It showcases fundamental principles of object-oriented programming in Python, allowing for the creation of robot instances.
+This Python program defines a Robot class that represents robots/device with attributes.
+It showcases principles of object-oriented programming in Python, allowing for the creation of robot/device instances.
 Also keeps track of the total number of robot instances created and provides a method to print their individual characteristics.
 
 ## Features
 
-- Create robot instances with different characteristics.
-- Track the total number of robots created.
-- Print robot characteristics using the `introduce_self()` method.
+- Create robot/device instances with different characteristics.
+- Track the total number of robots/devices.
+- Print robot/device characteristics using the `introduce_self()` method.
--- pycode.py
+++ pycode.py
@@ -2,28 +2,37 @@
 class Robot: 
     number_of_robots = 0
 
-    def __init__(self, name, color, brand, weight):
+    def __init__(self, name, color, brand, weight, swversion, ipaddress):
         self.name = name
         self.color = color
         self.brand = brand
         self.weight = float(weight)
+        self.swversion = (swversion)
+        self.ipaddress = (ipaddress)
 
         Robot.number_of_robots += 1
 
     # print information about their characteristics
     def introduce_self(self):
-        print(f"my characteristics are:\nName: {self.name}\nColor: {self.color}\nBrand: {self.brand}\nWeight: {self.weight} kg")
-
+        '''return a description of the device/robot'''
+        introduce = f'Name: {self.name}\n'\
+                    f'Color: {self.color}\n'\
+                    f'Brand: {self.brand}\n'\
+                    f'Weight: {self.weight} kg\n'\
+                    f'SWVersion: {self.swversion}\n'\
+                    f'IPAdress: {self.ipaddress}'
+        return introduce
         
-# add robots with their characteristics
-r1 = Robot('Romina', 'Black', 'Lefant', .500)
-r2 = Robot('Alexa', 'Gray', 'Amazon', .100)
+# add robots/devices with their characteristics
+r1 = Robot('Romina', 'Black', 'Lefant', .500, '4.7.1', '192.168.1.2')
+r2 = Robot('Alexa', 'Gray', 'Amazon', .100, '4.7.1', '192.168.1.3')
 
-# print number of robots. It will be updated as robots being added
+# print number of robots/devices. It will be updated as they being added
 print(f"the current number of robots is: {Robot.number_of_robots}")
    
-r1.introduce_self()
-r2.introduce_self()
+print('r1\n', r1.introduce_self(), '\n')
+print('r2\n', r2.introduce_self(), '\n')
+
 
 
 
